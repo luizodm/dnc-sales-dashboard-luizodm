54e67e0916a2cf727d448b5c5a8dcc1b5b039f91
luizodm <luizd.avila@hotmail.com>
2025-11-04 17:57:09 -0300

    feat(ui): adopt styled-components and set up routes index

---
+++ src/pages/Login.tsx
@@ -0,0 +1,23 @@
+import styled from 'styled-components'
+
+const LoginArea = styled.div`
+  background: #666;
+`
+
+const LoginImage = styled.div`
+  background-image: url(/login-image.svg);
+  backgroung-size: cover;
+  height: 100vh;
+  width: 50vh;
+`
+
+function Login() {
+  return (
+    <>
+      <LoginArea>LOGIN</LoginArea>
+      <LoginImage />
+    </>
+  )
+}
+
+export default Login
