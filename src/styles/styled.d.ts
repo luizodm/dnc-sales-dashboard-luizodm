9aece60a56c501e1510fb202b3e6600f70ba432f
luizodm <luizd.avila@hotmail.com>
2025-11-07 17:39:32 -0300

    feat(ui): header component

--- src/styles/styled.d.ts
+++ src/styles/styled.d.ts
@@ -5,6 +5,7 @@ declare module 'styled-components' {
   export interface DefaultTheme {
     appBackground: AppTheme['appBackground']
     appColor: AppTheme['appColor']
+    appDefaultStroke: AppTheme['appDefaultStroke']
     appLogo: AppTheme['appLogo']
     buttons: AppTheme['buttons']
     textInput: AppTheme['input']
