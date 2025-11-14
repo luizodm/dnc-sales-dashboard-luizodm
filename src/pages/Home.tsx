e7c883811bfe33c36a7ec8d3be218b456493668b
luizodm <luizd.avila@hotmail.com>
2025-11-14 18:52:45 -0300

    feat(ui): chart component

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,10 @@
-import { AvatarsLists, CardComponent, CustomTable, Header } from '@/components'
+import {
+  AvatarsLists,
+  CardComponent,
+  CustomChart,
+  CustomTable,
+  Header,
+} from '@/components'
 import { Container } from '@mui/material'
 import { currencyConverter } from '@/utils'
 
@@ -54,6 +60,13 @@ function Home() {
           <CustomTable
             headers={mockTableData.headers}
             rows={mockTableData.rows}
+          />
+        </CardComponent>
+        <CardComponent>
+          <CustomChart
+            labels={['Jan', 'Fev', 'Mar', 'Abr', 'Mai']}
+            data={[1000.12, 2456.54, 986.32, 654.89, 354.89]}
+            type="bar"
           />
         </CardComponent>
       </Container>
